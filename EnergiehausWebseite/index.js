c6ddc8d5f557581086545dedae11354c727fbc96
Wolfnet <wolfnetslab@gmail.com>
2023-04-07 16:00:25 +0200

    Added Java Variables

--- EnergiehausWebseite/index.js
+++ EnergiehausWebseite/index.js
@@ -9,3 +9,9 @@ function onInfo(){
 function logoClick(){
   location.replace("http://gisny.eu")
 }
+let temperature=6;
+let wind=6;
+let humidity=60;
+document.getElementById('temperature').innerHTML = temperature;
+document.getElementById('humidity').innerHTML = humidity;
+document.getElementById('wind').innerHTML = wind;

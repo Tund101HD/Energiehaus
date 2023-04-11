3be924c9ab24caeb01f9f64f928a823b9e434c1f
Tund_101_HD <luca-perri@outlook.de>
2023-04-11 15:18:37 +0200

    New file-structure; Reworked data.html

--- EnergiehausWebseite/index.js
+++ EnergiehausWebseite/index.js
@@ -1,14 +1,15 @@
 function onHome(){
-
+ window.location.href("index.html")
 }
 
-function onInfo(){
-
+function onData(){
+ window.location.replace("data.html")
 }
 
 function logoClick(){
   location.replace("http://gisny.eu");
 }
+
 let temperature=6;
 let wind=6;
 let humidity=60;

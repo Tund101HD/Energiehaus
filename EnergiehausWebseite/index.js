da06ece28095e39e3c9cc56f9943f8f48d849cb5
Tund_101_HD <luca-perri@outlook.de>
2023-05-19 14:22:53 +0200

    Added html-page for the weatherstation, added background files

--- EnergiehausWebseite/index.js
+++ EnergiehausWebseite/index.js
@@ -2,8 +2,16 @@ function onHome(){
  window.location.href("index.html")
 }
 
+function onReturn(){
+    window.location.replace("index.html")
+}
+
 function onData(){
  window.location.replace("data.html")
+}
+
+function onWetter(){
+    window.location.replace("wetterstation.html")
 }
 
 function logoClick(){
@@ -16,3 +24,13 @@ let humidity=60;
 document.getElementById('temperature').innerHTML = temperature;
 document.getElementById('humidity').innerHTML = humidity;
 document.getElementById('wind').innerHTML = wind;
+
+
+function loadBG(){
+    let rnd = Math.floor(Math.random()*10)+1;
+    document.getElementById("banner").style.backgroundImage = "url(imgs/Backgrounds/BG2.jpeg)";
+
+}
+
+
+
